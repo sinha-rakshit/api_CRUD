41ea137d096e5382379f6fc9fbbb38dff883af7b
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-08-01 18:17:33 +0530

    added delete option in database

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
 
-const users = [];
+let users = [];
 // all the routes in here are starting with /users
 router.get("/", (req, res) => {
   res.send(users);
@@ -21,8 +21,14 @@ router.get("/:id", (req, res) => {
   const { id } = req.params;
 
   const FoundUser = users.find((user) => user.id === id);
-
   res.send(FoundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with the id ${id} deleted from the database.`);
 });
 
 module.exports = router;
