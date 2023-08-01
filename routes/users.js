32ccbcbaaebd9bf6e90d2ef2e22ecb94cfb3e20c
sinha-rakshit <sinha.rakshit.16@gmail.com>
2023-08-01 15:06:23 +0530

    added push function in users route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const users = [
+  {
+    FirstName: "Aditya",
+    LastName: "Raj",
+    Age: 30,
+  },
+];
 // all the routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("Hello(from route:(user))");
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  console.log("Post Route Reached");
+
+  const user = req.body;
+  users.push(user);
+
+  res.send(`User with the name ${user.FirstName} added to the Database`);
 });
 
 module.exports = router;
